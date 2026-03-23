db8b8e9c5eada78012548fae0f63684358d6f40b
josipbarisic1 <josip.monopoly@gmail.com>
2026-03-23 18:52:21 +0100

    add CLI arguments and better logging for cleaner

--- automation/csv_cleaner/csv_cleaner.py
+++ automation/csv_cleaner/csv_cleaner.py
@@ -3,31 +3,48 @@
 # removes duplicates
 # and standardizes formatting.
 
+import os
 import csv
+import argparse
 
-path = "test_files"
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", help = "Path to input CSV file")
+parser.add_argument("--output",  help = "Path to output CSV file")
+args = parser.parse_args()
+
+BASE_DIR = os.path.dirname(__file__)
+reader_fallback = os.path.join(BASE_DIR, "test_files/users_messy.csv")
+writer_fallback = os.path.join(BASE_DIR, "test_files/users_clean.csv")
 
 def clean_csv():
     try:
-        with open(f"{path}/users_messy.csv", "r") as users_messy, open(f"{path}/users_clean.csv", "w") as users_clean:
+        if not args.input:
+            args.input = reader_fallback
+        if not args.output:
+            args.output = writer_fallback
+        with open(args.input, "r", encoding = "utf-8") as users_messy, open(args.output, "w", encoding = "utf-8") as users_clean:
+            print(f"[INFO] Reading from: {args.input}")
+            print(f"[INFO] Writing to: {args.output}")
             csvreader = csv.DictReader(users_messy)
             csvwriter = csv.DictWriter(users_clean, fieldnames = csvreader.fieldnames, lineterminator = "\n")
 
             set_uniques = set()
             csvwriter.writeheader()
             for row in csvreader:
-                for attribute in row:
-                    #print(f"|{row[e]}|")
-                    row[attribute] = row[attribute].strip().title()
+                for field in row:
+                    if row[field]:
+                        row[field] = row[field].strip().title()
                     
                 if tuple(row.values()) not in set_uniques:
                     set_uniques.add(tuple(row.values()))
                     csvwriter.writerow(row)
-            
+
+        print("[SUCCESS] CSV cleaned successfully")
+
     except FileNotFoundError:
-        print("File doesn't exist")
+        print("[ERROR] File doesn't exist")
     except IOError:
-        print("Failed to write data to file")
+        print("[ERROR] Failed to write data to file")
 
 if __name__ == "__main__":
     clean_csv()
